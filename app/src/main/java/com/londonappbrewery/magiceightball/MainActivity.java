253c831c08608ef040e151eb16a0d36273d99f09
Adeel Bhatti <bhattyadeel@hotmail.com>
2017-10-11 14:52:25 +1100

    Challenge Completed

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final int[] imgAnswers = {R.drawable.m1,
+                R.drawable.m2,
+                R.drawable.m3,
+                R.drawable.m4,
+                R.drawable.m5,
+                R.drawable.m6,
+                R.drawable.m7,
+                R.drawable.m8,
+                R.drawable.m9,
+                R.drawable.m10,
+                R.drawable.m11,
+                R.drawable.m12,
+                R.drawable.m13,
+                R.drawable.m14,
+                R.drawable.m15};
+
+        Button btnAsk = (Button) findViewById(R.id.btnAsk);
+        final ImageView ImgAns = (ImageView) findViewById(R.id.imageAnswer);
+
+
+        btnAsk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rndAnswerGenerator = new Random();
+                int rndAns = rndAnswerGenerator.nextInt(15);
+
+                ImgAns.setImageResource(imgAnswers[rndAns]);
+            }
+        });
     }
 }
